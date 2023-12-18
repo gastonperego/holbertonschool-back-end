1c90c110a5693537c205e33ec1736f9301f5a903
gastonperego <gastonperego@gmail.com>
2023-12-18 15:37:58 +0000

    fix: documentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """Prints the completed tasks of an employee"""
 
+if __name__ == '__main__':
+    from sys import argv
+    import urllib3
 
-from sys import argv
-import urllib3
 
+    users = urllib3.request("GET",
+                            f"https://jsonplaceholder.typicode.com/users?id={argv[1]}")
+    todos = urllib3.request("GET",
+                            f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}")
 
-users = urllib3.request("GET",
-                        f"https://jsonplaceholder.typicode.com/users?id={argv[1]}")
-todos = urllib3.request("GET",
-                        f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}")
+    users = users.json()
+    todos = todos.json()
 
-users = users.json()
-todos = todos.json()
+    num = 0
+    for todo in todos:
+        if todo['completed'] is True:
+            num += 1
 
-num = 0
-for todo in todos:
-    if todo['completed'] is True:
-        num += 1
+    print(f"Employee {users[0]['name']} is done with tasks({num}/{len(todos)})")
 
-print(f"Employee {users[0]['name']} is done with tasks({num}/{len(todos)})")
-
-for todo in todos:
-    if todo["completed"] is True:
-        print(f"    {todo['title']}")
+    for todo in todos:
+        if todo["completed"] is True:
+            print(f"    {todo['title']}")
